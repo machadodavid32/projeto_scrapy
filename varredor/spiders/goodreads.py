9d84c4a22f876c44e2cec8fc48b74de6736bc073
David <machadodavid32@gmail.com>
2022-12-07 19:16:17 -0300

    Terminando varredura de dados do sites goodreads em todas as paginas disponíveis

--- varredor/spiders/goodreads.py
+++ varredor/spiders/goodreads.py
@@ -1,11 +1,22 @@
 import scrapy
 
+# Extrair dados de varias páginas do doogreads.com
+# Para isso, utilize no terminal o scrapy shell, exemplo: scrapy shell 'https://www.goodreads.com/quotes?page=1'
+# Depois, digite: response.xpath("//a[@class='next_page']/@href").get()
+# Este é  resultado: '/quotes?page=2'  Nesta pagina, a unica diferença no link é o número da pagina. Portanto, precisamos apenas do numero da pagina para ir para a proxima pagina
+# response.xpath("//a[@class='next_page']/@href").get().split('=')
+# ['/quotes?page', '2']
+# response.xpath("//a[@class='next_page']/@href").get().split('=')[1]  # o primeiro indice é para a proxima pagina, que no caso é dois. Copie essa linha de comando e cole no codigo do programa
+#'2'
+#
+
+
 class GoodReadsSpider(scrapy.Spider):  # scrapy.spider para aproveitar as funcionalidades
     # identidade (nome do bot)
-    name = 'meubot2'
+    name = 'meubot3'
     # request
     def start_requests(self):
-        urls = ['https://www.goodreads.com/quotes']
+        urls = ['https://www.goodreads.com/quotes?page=1']  # reparar que eu peguei o primeiro link, da primeira pagina
         
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)  # yield serve para retornar diretamente para o site (se tivesse mais que um, sem o yield,...
@@ -20,19 +31,25 @@ class GoodReadsSpider(scrapy.Spider):  # scrapy.spider para aproveitar as funcio
         for elemento in response.xpath("//div[@class='quote']"):
             yield{
                 
-                'frase': elemento.xpath(".//span[@class='text']/text()").get(),  # para pegar o primeiro elemento. Reparar no ponto antes de //.
-                'autor': elemento.xpath(".//small[@class='author']/text()").get(),  # o ponto antes do // significa que queremos aquele conteúdo exato deste local do xptah
-                'tags': elemento.xpath(".//a[@class='tag']/text()").getall()  # Quero que pegue todas as tags do site
+                'frase': elemento.xpath(".//div[@class='quoteText']/text()").get(),  # para pegar o primeiro elemento. Reparar no ponto antes de //.
+                'autor': elemento.xpath(".//span[@class='authorOrTitle']/text()").get(),  # o ponto antes do // significa que queremos aquele conteúdo exato deste local do xptah
+                'tags': elemento.xpath(".//div[@class='greyText smallText left']/text()").getall()  # Quero que pegue todas as tags do site
             }
             
             
             
             # Tentar encontrar o botão próximo, se encontrar, vou varrer essas páginas.
-        try:
-            next_page = response.xpath("//a[@class='next_page']").get()
-            if next_page is not None:
-                full_next_page = response.urljoin(next_page)
-                yield scrapy.Request(url=full_next_page, callback=self.parse)
-        except:
-            print("Chegamos na ultima página")
-                    
+        prox_pag = response.xpath("//a[@class='next_page']/@href").get().split('=')[1]
+        print('#'*10) # Isso é para mostrar no terminal, de forma clara, se as paginas estão realmente sendo acessadas.
+        print(prox_pag)
+        print('#'*20)
+        if prox_pag is not None:
+            full_next_page = f'https://www.goodreads.com/quotes?page={prox_pag}'
+            print('#'*20)
+            print(full_next_page)
+            print("#"*20)
+            yield scrapy.Request(url=full_next_page, callback=self.parse)
+        
+                
+                
+                
